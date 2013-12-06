6828847a75eba71f303baf04b31ffa73fa012126
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-06 14:46:28 -0700

    add about page. write tests for home and help page. all tests passing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,26 @@
 require 'spec_helper'
 
-describe "Home Page" do
-  it "should have the content 'Sample App'" do 
-    visit '/static_pages/home'
-    page.should have_content('Sample App')
+describe "Static Pages" do
+
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do 
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
   end
+
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "About us  Page" do
+    it "Should have the content 'About us'" do
+      visit '/static_pages/about'
+      page.should have_content('About us')
+    end
+  end
+
 end
