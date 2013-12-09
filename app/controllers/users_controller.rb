cb1e25478a66cc2ac23d3689a9b23b355c4cf0c6
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-09 13:01:24 -0700

    flash message for signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
 #these are the same.
       #redirect_to user_path(@user)
       redirect_to @user
