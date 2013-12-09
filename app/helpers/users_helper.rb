9ac3b7a7587fb3d8bb933ad74b033a6f4691a636
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-08 19:04:52 -0700

    working user show page. gravatar and tests. also useless helper methods.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
+  def reverse_name(user)
+    user.name.reverse
+  end
+  
+  def kitten
+    alt = ["Meow", "Who me?", "Kittez", "moar kittez"]
+    image_tag("http://placekitten.com/100/#{rand(100..300)}",
+    title: alt.sample)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp2::Application.routes.draw do
 
-  get "users/new"
+resources :users
+# get "users/new"
 
   root :to => 'static_pages#home'
   match '/help', to: 'static_pages#help'
