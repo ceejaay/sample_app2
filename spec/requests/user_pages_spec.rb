7ae8e29150fceb195e34ab98f2504a9c70d8c0e1
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-10 14:52:48 -0700

    update links to edit page. Still needs update method in user controller page 380

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 describe "User Pages" do
   subject {page}
 
+  describe "edit" do 
+    let(:user) {FactoryGirl.create(:user) }
+    before {visit edit_user_path(user)}
+    
+    describe "page" do
+      it {should have_selector('h1', text: "Update your profile")}
+      it {should have_selector('title', text: "Edit user")}
+      it {should have_link('change', href: "http://gravatar.com/emails") }
+    end
+    
+    describe "with invalid information" do
+      before {click_button "Save changes"}
+      it {should have_content('error')}
+    end
+  end
+
+
   describe "profile page" do
    let(:user) {FactoryGirl.create(:user)}
    before {visit user_path(user)}
