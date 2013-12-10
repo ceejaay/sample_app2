1d17cf43126aa36c1356e478081b6b302dc9e0e9
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-09 20:37:23 -0700

    ended on pg. 349 with adding create remember token in user model and private method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,21 @@ describe User do
 
   subject {@user}
 
+
+
   it {should respond_to(:name)}
   it {should respond_to(:email)}
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token)}
   it {should respond_to(:authenticate)}
   it {should be_valid}
+
+  describe "remember token" do
+    before {@user.save}
+    its(:remember_token) {should_not be_blank}
+  end
 
   describe "With password that is too short" do
     before { @user.password = @user.password_confirmation = "a" * 5}
